397bc5b16dadb53074490ba1313cb465c6778c64
quantumloops <158140786+quantumloops@users.noreply.github.com>
2025-07-18 14:15:46 +0530

    Delete Villa Vista/wrapAsync.js

--- Villa Vista/wrapAsync.js
+++
@@ -1,7 +0,0 @@
-const wrapAsync = (fn) => {
-    return (req, res, next) => {
-        fn(req, res, next).catch(next); // Directly call .catch on the result of fn
-    };
-};
-
-module.exports = wrapAsync;
